7055f89986b57bec0dc459c543ef746ace743823
Ryan Healy <ryanhealy1992@gmail.com>
2019-02-23 23:39:22 +0900

    update user profile functionality added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.decorators import login_required
-
+from django.views.generic import ListView
+from django.views.generic import DetailView
 # Create your views here.
 @login_required(login_url="/login")
 def home(request):
@@ -9,6 +10,15 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
